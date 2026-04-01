540be5d95796ffc025cc886ecb1f35d2b8694adb
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-04-01 21:38:12 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.css'
+const App = () => {
+  return (
+    <main className='h-screen w-full bg-zinc-900 flex gap-2 p-2'>
+        <aside className='w-1/6 bg-zinc-400 rounded-md'></aside>
+        <section className='w-5/6 bg-zinc-800 rounded-md'></section>
+    </main>
+  )
+}
+
+export default App
